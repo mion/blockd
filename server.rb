7c5822a9413287a05d0076bf8e1cad8dd8aeaba0
Gabriel Vieira <gluisvieira@gmail.com>
2017-08-27 17:14:39 -0300

    Version 1.0.2

--- server.rb
+++ server.rb
@@ -1,4 +1,22 @@
 require 'sinatra'
+require 'pry'
+
+class FilenameSanitizer
+  attr_reader :filename
+
+  def initialize(filename)
+    @filename = if filename.nil? || filename.empty?
+      "unnamed"
+    else
+      filename
+    end
+  end
+
+  def sanitize
+    # binding.pry
+    filename.strip.downcase.gsub(/\W+|\s+/, '-').gsub(/\A\-|\-\Z/, '')
+  end
+end
 
 get '/' do
   File.read(File.join('public', 'index.html'))
@@ -6,10 +24,13 @@ end
 
 post '/blocks' do
   payload = JSON.parse(request.body.read)
-  puts payload["filename"]
-  puts payload["paragraphs"]
+  puts "[*] Raw payload: #{payload}"
+  filename = FilenameSanitizer.new(payload["filename"]).sanitize.concat("-#{Time.now.to_i}")
+  paragraphs = payload["paragraphs"].join("\n")
   path = "/Users/gvieira/Documents/blockd"
-  payload["filename"] = "unnamed" if payload["filename"] == ''
-  fname = payload["filename"].sub(" ", "-") + "-#{Time.now.to_i}"
-  File.open(path + "/" + fname + ".txt", "w+") { |f| f.puts(payload["paragraphs"].join("\n")) }
+  puts "[*] Path: #{path}"
+  puts "[*] Filename: #{filename}"
+  puts "[*] Paragraphs: #{paragraphs}"
+  File.open(path + "/" + filename + ".txt", "w+") { |f| f.puts(paragraphs) }
 end
+
